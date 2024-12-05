fbdab36ba7cd5e9eeea05dad5e644b20353bc0c7
mariana f. <91221958+marianaolfr@users.noreply.github.com>
2024-12-04 22:44:06 -0300

    Update classroom.py

--- classroom.py
+++ classroom.py
@@ -1,12 +1,12 @@
 class Classroom:
     def __init__(self):
-        self.students = []
+        self.classroom = []
         self.lowest_grade = None
         self.highest_grade = None
 
     def add_students(self, students):
         for student in students:
-            self.students.append(student)
+            self.classroom.append(student)
             if self.lowest_grade is None or self.lowest_grade.grade > student.grade:
                 self.lowest_grade = student
             if self.highest_grade is None or self.highest_grade.grade < student.grade:
@@ -14,5 +14,5 @@ class Classroom:
 
     def show_students(self):
         print("Number of students:", len(self.students))
-        for student in self.students:
+        for student in self.classroom:
             print(student.show_student())
